df237640d42f5fd56cbabce4bf8a5b6a168d21a5
uabbsm <48964451+uabbsm@users.noreply.github.com>
2020-03-16 12:06:23 +0000

    Initial commit

---
+++ app/src/main/java/com/example/popularmoviesapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.popularmoviesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //created movie class
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
