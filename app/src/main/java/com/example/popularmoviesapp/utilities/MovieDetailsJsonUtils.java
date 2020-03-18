675410daf156667b8e41e9b2e2db41df3a7b784c
uabbsm <48964451+uabbsm@users.noreply.github.com>
2020-03-18 16:11:33 +0000

    JsonAndNetworkUtils
    *networkuitls
    *json util
    *movie movel
    * main activity modifications

---
+++ app/src/main/java/com/example/popularmoviesapp/utilities/MovieDetailsJsonUtils.java
@@ -0,0 +1,46 @@
+package com.example.popularmoviesapp.utilities;
+
+import android.content.Context;
+
+import com.example.popularmoviesapp.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MovieDetailsJsonUtils {
+
+    private static final String RESULTS = "results";
+    private static final String TITLE = "title";
+    private static final String POSTER_PATH = "poster_path";
+    private static final String RELEASE_DATE = "release_date";
+    private static final String VOTE_AVERAGE = "vote_average";
+    private static final String OVERVIEW = "overview";
+
+    private static final String POSTER_BASE_ULT = "http://image.tmdb.org/t/p/";
+    private static final String POSTER_SIZE = "w185";
+
+    public static Movie[] getSimpleInfoStringsFromJson(Context context, String json) throws JSONException {
+
+        JSONObject moviesJson = new JSONObject(json);
+        JSONArray moviesResultJsonArray = moviesJson.getJSONArray(RESULTS);
+
+        Movie[] moviesDetailsArray = new Movie[moviesResultJsonArray.length()];
+
+        for (int i = 0; i < moviesResultJsonArray.length(); i++) {
+
+            String movieTitle = moviesResultJsonArray.getJSONObject(i).optString(TITLE);
+            String moviePoster = moviesResultJsonArray.getJSONObject(i).optString(POSTER_PATH);
+            String movieRelease = moviesResultJsonArray.getJSONObject(i).optString(RELEASE_DATE);
+            String movieRate = moviesResultJsonArray.getJSONObject(i).optString(VOTE_AVERAGE);
+            String movieOverview = moviesResultJsonArray.getJSONObject(i).optString(OVERVIEW);
+
+            moviesDetailsArray[i] = new Movie(movieTitle,
+                    POSTER_BASE_ULT + POSTER_SIZE + moviePoster,
+                    movieRelease,
+                    movieRate,
+                    movieOverview);
+        }
+        return moviesDetailsArray;
+    }
+}
