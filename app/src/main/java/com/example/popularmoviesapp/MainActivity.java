092aae155205a4a044e93103d162f6eac5891126
uabbsm <48964451+uabbsm@users.noreply.github.com>
2020-03-19 17:05:59 +0000

    Revert "Revert "Update MainActivity.java""
    This reverts commit b3eedf5bbc65792f090f1da434b2b857e1c65908.

--- app/src/main/java/com/example/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.content.Context;
@@ -44,12 +46,12 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
         mLoadingIndicator = (ProgressBar) findViewById(R.id.progressbar);
         mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
 
-<<<<<<< Updated upstream
+
         mLayoutManager = new GridLayoutManager(this, 100);
         mMoviesAdapter = new MoviesAdapter(mMovies,this);
-=======
+
         GridLayoutManager LayoutManager = new GridLayoutManager(this, 2);
->>>>>>> Stashed changes
+
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setHasFixedSize(true);
@@ -66,12 +68,11 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
     @Override
     public void onListItemClick(int item){
         Context context = this;
-<<<<<<< Updated upstream
-=======
-        Intent intent = new Intent(this, MovieDetailsActivity.class);\
+
+        Intent intent = new Intent(this, MovieDetailsActivity.class);
         intent.putExtra("Movie", mMovies[item]);
         startActivity(intent);
->>>>>>> Stashed changes
+
         Toast.makeText(context, "Item nº: " + item + " has been clicked", Toast.LENGTH_SHORT)
                 .show();
     }
