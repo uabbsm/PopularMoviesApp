ba7f3a1c02fa0bbaf185ce27dfbbcd824aa40d5f
uabbsm <48964451+uabbsm@users.noreply.github.com>
2020-03-19 16:02:31 +0000

    Revert "Revert "Fix errors""
    This reverts commit a137b8e3b1a0570e433f1f8aa9615cd6bc0e17e5.

--- app/src/main/java/com/example/popularmoviesapp/MoviesAdapter.java
+++ app/src/main/java/com/example/popularmoviesapp/MoviesAdapter.java
@@ -3,6 +3,7 @@ package com.example.popularmoviesapp;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
